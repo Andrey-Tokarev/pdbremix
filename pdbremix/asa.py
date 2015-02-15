840c191687682a1033d92609c45a5ee5d970838f
Andrey-Tokarev <andrei.tokarev@gmail.com>
2015-02-15 12:31:20 +0200

    Update asa.py
    Optimized "calculate_asa_opt" works in O(n * m + n * N * m) time complexity in comparison to original version "calculate_asa_opt" that works in O(n * n + n * N * m), where n - number of atoms = len(atoms), m - averaged number of neighbors of an atom = <len(neighbor_indices)>, N - number of generated sphere points = n_sphere_point. Thus, optimization is linear in number of atoms by  "pre-partition the atoms into disjoint cubes to speed up the search for neighboring atoms" suggested by Boscoh K.H. Another optimization is in using square of distances instead of distances in two places: this function and "find_neighbor_indices_mod", because of avoiding heavy square root operation. For this purpose new function "mag2" (square of vector magnitude) was added to two files: v3numpy.py and v3array.py.

--- pdbremix/asa.py
+++ pdbremix/asa.py
@@ -17,6 +17,7 @@ import math
 
 import v3
 import pdbatoms
+from collections import defaultdict
 
 
 def generate_sphere_points(n):
@@ -81,6 +82,140 @@ def calculate_asa(atoms, probe, n_sphere_point=960):
         r = atom_j.radius + probe
         diff = v3.distance(atom_j.pos, test_point)
         if diff*diff < r*r:
+          j_closest_neighbor = j
+          is_accessible = False
+          break
+      if is_accessible:
+        n_accessible_point += 1
+    
+    area = const*n_accessible_point*radius*radius 
+    areas.append(area)
+
+  return areas
+
+
+def makeBoxes(A, dmax):
+    '''
+    returns dictionary which keys are indecies of boxes (regions)
+    with dmax length side and values
+    are indicies of atoms belonging to these boxes
+    '''
+    B = defaultdict(list) # space divided into boxes
+    for i in xrange(len(A)):
+        atom = A[i]
+        boxCoor = tuple(int(math.floor(x / dmax)) for x in atom.pos)
+        B[boxCoor].append(i)
+    return B
+
+
+def addBond(A, a1, a2, conn, dmax):
+    '''
+    add bond (if its length satisfying bond limits)
+    to atoms a1 and a2 in adjacency list conn
+    '''
+    atom1 = A[a1]
+    atom2 = A[a2]
+    if v3.mag2(atom1.pos - atom2.pos) <= dmax * dmax:  # connected
+        conn[a1].append(a2)
+        conn[a2].append(a1)
+
+
+def neighbAtoms(B, box):
+    '''
+    returns list of atoms from half of neighbouring boxes of the box
+    another half is accounted when symmetric (opposite) boxes considered
+    '''
+    na = [] # list for neighboring atoms
+    x, y, z = box # coordinates of the box
+    # top layer consisting of 9 boxes
+    if (x + 1, y + 1, z +1) in B: na.extend(B[(x + 1, y + 1, z +1)])
+    if (x, y + 1, z +1) in B: na.extend(B[(x, y + 1, z +1)])
+    if (x + 1, y, z +1) in B: na.extend(B[(x + 1, y, z +1)])
+    if (x, y, z +1) in B: na.extend(B[(x, y, z +1)])
+    if (x - 1, y + 1, z +1) in B: na.extend(B[(x - 1, y + 1, z +1)])
+    if (x + 1, y - 1, z +1) in B: na.extend(B[(x + 1, y - 1, z +1)])
+    if (x, y - 1, z +1) in B: na.extend(B[(x, y - 1, z +1)])
+    if (x - 1, y, z +1) in B: na.extend(B[(x - 1, y, z +1)])
+    if (x - 1, y - 1, z +1) in B: na.extend(B[(x - 1, y - 1, z +1)])
+    # half of the middle layer excluding the box itself (4 boxes)
+    if (x + 1, y + 1, z) in B: na.extend(B[(x + 1, y + 1, z)])
+    if (x, y + 1, z) in B: na.extend(B[(x, y + 1, z)])
+    if (x + 1, y, z) in B: na.extend(B[(x + 1, y, z)])
+    if (x + 1, y - 1, z) in B: na.extend(B[(x + 1, y - 1, z)])
+    return na
+
+
+def adjList(A, dmax):
+    '''
+    returns adjacency list from coordinate file
+    in O(len(A)) time!
+    '''
+    B = makeBoxes(A, dmax) # put atoms into the boxes with dmax length side
+    # now go on boxes and check connections inside 3x3 superboxes
+    conn = [[] for i in xrange(len(A))] # list of bond lengths each atom implicated
+    for box in B:
+        lb = len(B[box])
+        for i in range(lb):
+            a1 = B[box][i]
+            # check possible connections inside the box
+            for j in range(i+1, lb):
+                a2 = B[box][j]
+                addBond(A, a1, a2, conn, dmax)
+            # check connections with atoms from neighbouring boxes
+            na = neighbAtoms(B, box) # list of such atoms
+            for a2 in na:
+                addBond(A, a1, a2, conn, dmax)
+    return conn
+
+
+def find_neighbor_indices_mod(atoms, indices, probe, k):
+  """
+  Returns list of indices of atoms within probe distance to atom k. 
+  """
+  neighbor_indices = []
+  atom_k = atoms[k]
+  radius = atom_k.radius + probe + probe
+  for i in indices:
+    if i == k: continue
+    atom_i = atoms[i]
+    dist2 = v3.mag2(atom_k.pos - atom_i.pos) # ToAn
+    if dist2 < (radius + atom_i.radius) ** 2: # ToAn
+      neighbor_indices.append(i)
+  return neighbor_indices
+
+
+def calculate_asa_opt(atoms, probe, n_sphere_point=960):
+  """
+  Optimized version
+  Returns the accessible-surface areas of the atoms, by rolling a
+  ball with probe radius over the atoms with their radius
+  defined.
+  """
+  sphere_points = generate_sphere_points(n_sphere_point)
+ 
+  const = 4.0 * math.pi / len(sphere_points)
+  areas = []
+  neighbor_list = adjList(atoms, 2 * (probe + max(atoms, key=lambda p: p.radius).radius))
+  for i, atom_i in enumerate(atoms):
+    
+    neighbor_indices = [neig for neig in neighbor_list[i]]
+    neighbor_indices = find_neighbor_indices_mod(atoms, neighbor_indices, probe, i) # even further narrow diapazon
+    n_neighbor = len(neighbor_indices)
+    j_closest_neighbor = 0
+    radius = probe + atom_i.radius
+    
+    n_accessible_point = 0
+    for point in sphere_points:
+      is_accessible = True
+      test_point = v3.scale(point, radius) + atom_i.pos
+      cycled_indices = range(j_closest_neighbor, n_neighbor)
+      cycled_indices.extend(range(j_closest_neighbor))
+      
+      for j in cycled_indices:
+        atom_j = atoms[neighbor_indices[j]]
+        r = atom_j.radius + probe
+        diff2 = v3.mag2(atom_j.pos - test_point)
+        if diff2 < r*r:
           j_closest_neighbor = j
           is_accessible = False
           break
